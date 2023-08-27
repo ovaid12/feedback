38bc2a3f66f7acf625df48194fa01849b5cd6556
ovaid12 <90675108+ovaid12@users.noreply.github.com>
2023-08-27 17:08:45 +0530

    Update databaseConnection.js

--- backend/config/database/databaseConnection.js
+++ backend/config/database/databaseConnection.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose');
-const db = mongoose.connect("mongodb+srv://Ovaid:Ovaid123@cluster0.qcv5rah.mongodb.net/Training").then(()=>console.log("Connection Successful")).catch((error)=>console.log(error))
+const db = mongoose.connect("mongo_url").then(()=>console.log("Connection Successful")).catch((error)=>console.log(error))
 module.exports = db
